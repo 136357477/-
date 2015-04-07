0025532e73b9eec0b05fd4eb13403486033973a8
136357477 <guohui136357477@126.com>
2015-04-07 23:38:48 +0800

    平时整理的有用的文档
    AFNetWork的详解

---
+++ 文档/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  文档
+//
+//  Created by guohui on 15/4/7.
+//  Copyright (c) 2015年 youyibao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 文档/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  文档
+//
+//  Created by guohui on 15/4/7.
+//  Copyright (c) 2015年 youyibao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
