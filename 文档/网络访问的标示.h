ff7fdd8e3929c4e8bb6335d4a40763786524266f
136357477 <guohui136357477@126.com>
2015-04-09 11:28:33 +0800

    网络访问标示

---
+++ 文档/网络访问的标示.h
@@ -0,0 +1,10 @@
+//
+//  网络访问的标示.h
+//  文档
+//
+//  Created by guohui on 15/4/9.
+//  Copyright (c) 2015年 youyibao. All rights reserved.
+//
+
+// 网络访问信号：
+[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
