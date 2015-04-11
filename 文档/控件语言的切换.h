c76ba25c3e42e13f535c8fcc4faca0bf77559f0c
136357477 <guohui136357477@126.com>
2015-04-11 17:10:52 +0800

    实现语言的切换
    控件的语言不切换

---
+++ 文档/控件语言的切换.h
@@ -0,0 +1,11 @@
+//
+//  控件英文不转换为中文.h
+//  文档
+//
+//  Created by guohui on 15/4/11.
+//  Copyright (c) 2015年 youyibao. All rights reserved.
+//
+点击工程名,
+在Localizations默认只有English这一项,
+我们只需在Localizations上添加Chinese就可以了,
+你还可以根据你的需要添加其他的语言.
