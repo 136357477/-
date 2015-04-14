6a7e4458f1161d036f5553dbb859247f99d596c7
136357477 <guohui136357477@126.com>
2015-04-14 13:24:34 +0800

    iOS程序语言的切换

--- 文档/控件语言的切换.h
+++ 文档/控件语言的切换.h
@@ -9,3 +9,5 @@
 在Localizations默认只有English这一项,
 我们只需在Localizations上添加Chinese就可以了,
 你还可以根据你的需要添加其他的语言.
+
+如果不行 可以设置Supporting Files文件下的 Info.plist 将Localization native development region 的值设置为China  即可为中文
